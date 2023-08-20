9c5fdb39b29240702012e30a5d0a8748a3a5347d
Krisztian Bernath <70200514+DrKrisz@users.noreply.github.com>
2023-08-21 01:11:31 +0200

    index

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 function toggleDarkMode() {
     document.body.classList.toggle('dark');
+    updateButtonContent();
+}
+
+function updateButtonContent() {
+    const button = document.querySelector('.toggle-button');
+    if (document.body.classList.contains('dark')) {
+        button.textContent = 'Light Mode';
+    } else {
+        button.textContent = 'Dark Mode';
+    }
 }
